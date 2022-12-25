ad72d87ec5b4b9e73e61ed7fd30fa7d7d79c247b
Florian Thiévent <florianthievent@MBP.fritz.box>
2022-12-25 14:19:53 +0100

    initial

---
+++ src/main/kotlin/org/thievent/abimees/AbimeesApplication.kt
@@ -0,0 +1,11 @@
+package org.thievent.abimees
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AbimeesApplication
+
+fun main(args: Array<String>) {
+	runApplication<AbimeesApplication>(*args)
+}
