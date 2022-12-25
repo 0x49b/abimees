4da5170c7281d3125883994e6d9678276b363eaf
Florian Thiévent <florianthievent@MBP.fritz.box>
2022-12-25 14:26:30 +0100

    change kotlin/gradle to java/maven

---
+++ src/main/java/org/thievent/abimees/AbimeesApplication.java
@@ -0,0 +1,13 @@
+package org.thievent.abimees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbimeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbimeesApplication.class, args);
+	}
+
+}
